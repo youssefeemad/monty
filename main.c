6c68dae97a48b4ef179f6f17482452fcb49c8ec2
youssefeemad <eyoussef228@gmail.com>
2023-12-14 19:10:26 +0200

    new

--- main.c
+++ main.c
@@ -1,83 +1,26 @@
+// main.c
 #include "monty.h"
-stack_t *head = NULL;
-
-/**
- * main - entry point
- * @argc: arguments count
- * @argv: list of arguments
- * Return: always 0
- */
 
 int main(int argc, char *argv[])
 {
+	// Check for correct number of arguments
 	if (argc != 2)
 	{
 		fprintf(stderr, "USAGE: monty file\n");
 		exit(EXIT_FAILURE);
 	}
-	open_file(argv[1]);
-	free_nodes();
-	return (0);
-}
 
-/**
- * create_node - Creates a node.
- * @n: Number to go inside the node.
- * Return: Upon sucess a pointer to the node. Otherwise NULL.
- */
-stack_t *create_node(int n)
-{
-	stack_t *node;
-
-	node = malloc(sizeof(stack_t));
-	if (node == NULL)
-		err(4);
-	node->next = NULL;
-	node->prev = NULL;
-	node->n = n;
-	return (node);
-}
-
-/**
- * free_nodes - Frees nodes in the stack.
- */
-void free_nodes(void)
-{
-	stack_t *tmp;
-
-	if (head == NULL)
-		return;
-
-	while (head != NULL)
+	// Open file
+	FILE *file = fopen(argv[1], "r");
+	if (!file)
 	{
-		tmp = head;
-		head = head->next;
-		free(tmp);
-	}
-}
-
-
-/**
- * add_to_queue - Adds a node to the queue.
- * @new_node: Pointer to the new node.
- * @ln: line number of the opcode.
- */
-void add_to_queue(stack_t **new_node, __attribute__((unused))unsigned int ln)
-{
-	stack_t *tmp;
-
-	if (new_node == NULL || *new_node == NULL)
+		fprintf(stderr, "Error: Can't open file %s\n", argv[1]);
 		exit(EXIT_FAILURE);
-	if (head == NULL)
-	{
-		head = *new_node;
-		return;
 	}
-	tmp = head;
-	while (tmp->next != NULL)
-		tmp = tmp->next;
 
-	tmp->next = *new_node;
-	(*new_node)->prev = tmp;
+	// TODO: Read and parse the Monty byte code file, execute instructions
 
+	fclose(file);
+
+	return 0;
 }
