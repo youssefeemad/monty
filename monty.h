b21893f243b3bca42140e837e198fbc6232a4449
youssefeemad <eyoussef228@gmail.com>
2023-12-14 18:54:33 +0200

    new

--- monty.h
+++ monty.h
@@ -35,6 +35,7 @@ typedef struct instruction_s
 	void (*f)(stack_t **stack, unsigned int line_number);
 } instruction_t;
 
+extern stack_t *head;
 // Function prototypes
 void push(stack_t **stack, int value, unsigned int line_number);
 void pall(stack_t **stack, unsigned int line_number);
